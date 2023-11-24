c230b5df178e5fccb8fbbf5977ff1d3fcf9797eb
444B <15311579+444B@users.noreply.github.com>
2023-11-24 22:07:02 +0100

    testing st.button

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import streamlit as st
 
 st.header('st.button')
 
-if st.button('Say hello'):
+if st.button('Say hello', help='This is the help string'):
      st.write('Why hello there')
 else:
      st.write('Goodbye')
+
+st.button('Primary', type='primary')
+st.button('Secondary', type='secondary')
+st.button('On click', key='on_click', on_click=lambda: st.write('Clicked!'))
